c1cc55a0553b909891f844b750e39a34218b4114
Robin Berjon <robin.berjon@nytimes.com>
2026-06-10 12:03:30 +0200

    progress

--- src/el/model-header.js
+++ src/el/model-header.js
@@ -1,7 +1,9 @@
 
 import { LitElement, html, css, nothing } from 'lit';
 import { SignalWatcher } from '@lit-labs/signals';
+import '@supramundane/ui/input';
 import { setTileName, activeTab } from '../state.js';
+
 
 class ModelHeader extends SignalWatcher (LitElement) {
   static styles = css`
@@ -22,6 +24,7 @@ class ModelHeader extends SignalWatcher (LitElement) {
   // For now it's only the name, we can add description and banner later.
   render () {
     const masl = activeTab()?.masl;
+    console.warn(`masl`, masl);
     if (!masl?.model) return nothing;
     return html`
       <form class="model-header" @submit=${this.#handleSubmit}>
